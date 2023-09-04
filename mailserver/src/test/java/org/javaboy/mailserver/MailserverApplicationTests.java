4abead0b93da7662b1e7052ab4f1bb8dc3e535f3
itcast <1097917686@qq.com>
2023-09-04 21:51:01 +0800

    完善代码。

--- mailserver/src/test/java/org/javaboy/mailserver/MailserverApplicationTests.java
+++ mailserver/src/test/java/org/javaboy/mailserver/MailserverApplicationTests.java
@@ -1,13 +1,10 @@
 package org.javaboy.mailserver;
 
-import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MailserverApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
